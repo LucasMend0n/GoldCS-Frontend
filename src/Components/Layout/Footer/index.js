ba33c4843c6a219acffedcba17edac5d399597af
Lucas Mendonça da Silva junior <lucasjr413@gmail.com>
2023-05-14 17:46:50 -0300

    Add Navbar and Footer

--- src/Components/Layout/Footer/index.js
+++ src/Components/Layout/Footer/index.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
+import './Footer.model.css'
+import { AiFillGithub } from 'react-icons/ai'
 
 export class Footer extends Component {
   render() {
     return (
-      <div>Footer</div>
+      <footer className='footer'>
+
+        <ul className="social_list">
+          <li>
+            <a target='blank' href="https://github.com/JoaopedroSassi"><AiFillGithub/></a>
+          </li>
+          <li>
+            
+            <a target='blank' href="https://github.com/FelipeFelixhub"><AiFillGithub/></a>
+          </li>
+          <li>
+            <a target='blank' href="https://github.com/LucasMend0n"><AiFillGithub/></a>
+          </li>
+        </ul>
+        <p><span>Gold Colchoes</span> &copy; 2023</p>
+      </footer>
     )
   }
 }
--- src/Components/Layout/Navbar/index.js
+++ src/Components/Layout/Navbar/index.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import Container from '../Container'
+import './Navbar.model.css'
+import logo from '../../../Assets/goldLogo.png'
 
 export class Navbar extends Component {
   render() {
     return (
-        <ul>
-            <Link to="/">Home</Link>
-            <Link to="/amount">Estoque</Link>
-            <Link to="/product">Produto</Link>
-            <Link to="/order">Pedido</Link>
-        </ul>
+      <nav className="navbar">
+        <Container customClass="max-width">
+          <Link to="/"><img src={logo} alt="Logo gold" /></Link>
+          <ul className="list">
+            <li className='item'>
+              <Link to="/">Home</Link>
+            </li>
+            <li className='item'>
+              <Link to="/amount">Estoque</Link>
+            </li>
+            <li className='item'>
+              <Link to="/product">Produto</Link>
+            </li>
+            <li className='item'>
+              <Link to="/order">Pedido</Link>
+            </li>
+          </ul>
+        </Container>
+      </nav>
     )
   }
 }
