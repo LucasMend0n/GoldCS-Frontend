0cac464a33b49187a52c6de47f3fba61e18a1580
Lucas Mendonça da Silva Junior <lucasjr413@gmail.com>
2023-10-08 20:54:46 -0300

    DESENV-013 - Alterando icone de pedidos e finalizando homepage

--- src/Components/Pages/Home/index.jsx
+++ src/Components/Pages/Home/index.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 import './Home.css';
 import { getUserLocalStorage } from '../../../context/util';
 import { Link, NavLink } from 'react-router-dom';
-import { AiFillHome, AiOutlineUser } from 'react-icons/ai'
-import { BsFillBoxSeamFill, BsFillArchiveFill } from 'react-icons/bs'
-import { BiNotepad } from 'react-icons/bi'
+import { AiOutlineUser } from 'react-icons/ai'
+import { BsFillBoxSeamFill, BsFillArchiveFill, BsFillFileTextFill } from 'react-icons/bs'
 
 
 const Home = () => {
@@ -12,28 +11,25 @@ const Home = () => {
   return (
     <section className="HomePage">
       <div className='display-user'>Bem vindo, {user.name}</div>
-      <div className='home_buttons'>
-        <NavLink id='order' className={'btn-global btn-home'} to={'/order'}>
-          <BiNotepad />
-          Pedido
-        </NavLink>
-        <NavLink className={'btn-global btn-home'} to={'/product'}>
-          <BsFillBoxSeamFill />
-          Produto
-        </NavLink>
-        <NavLink className={'btn-global btn-home'} to={'/amount'}>
-          <BsFillArchiveFill />
-          Estoque
-        </NavLink>
-        <NavLink className={'btn-global btn-home'} to={'/profile'}>
-          <AiOutlineUser />
-          Perfil
-        </NavLink>
-      </div>
-      <article>
-        <Link to={'/newOrder'}>
-          <button>Novo Pedido</button>
-        </Link>
+      <article className='buttons_section'>
+        <div className='home_buttons'>
+          <NavLink id='order' className={'btn-global btn-home'} to={'/order'}>
+            <BsFillFileTextFill />
+            Pedido
+          </NavLink>
+          <NavLink className={'btn-global btn-home'} to={'/product'}>
+            <BsFillBoxSeamFill />
+            Produto
+          </NavLink>
+          <NavLink className={'btn-global btn-home'} to={'/amount'}>
+            <BsFillArchiveFill />
+            Estoque
+          </NavLink>
+          <NavLink className={'btn-global btn-home'} to={'/profile'}>
+            <AiOutlineUser />
+            Perfil
+          </NavLink>
+        </div>
 
       </article>
     </section>
