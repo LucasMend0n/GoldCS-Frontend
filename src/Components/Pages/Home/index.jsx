b2f036d9aacb033d9db822be01a7054daa52d021
Lucas Mendonça da Silva Junior <lucasjr413@gmail.com>
2023-10-08 19:55:21 -0300

    DESENV-013 - Implemtentação do novo design da página home

--- src/Components/Pages/Home/index.jsx
+++ src/Components/Pages/Home/index.jsx
@@ -1,16 +1,36 @@
 import React from 'react'
 import './Home.css';
 import { getUserLocalStorage } from '../../../context/util';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import { AiFillHome, AiOutlineUser } from 'react-icons/ai'
+import { BsFillBoxSeamFill, BsFillArchiveFill } from 'react-icons/bs'
+import { BiNotepad } from 'react-icons/bi'
 
 
 const Home = () => {
   const user = getUserLocalStorage()
   return (
     <section className="HomePage">
+      <div className='display-user'>Bem vindo, {user.name}</div>
+      <div className='home_buttons'>
+        <NavLink id='order' className={'btn-global btn-home'} to={'/order'}>
+          <BiNotepad />
+          Pedido
+        </NavLink>
+        <NavLink className={'btn-global btn-home'} to={'/product'}>
+          <BsFillBoxSeamFill />
+          Produto
+        </NavLink>
+        <NavLink className={'btn-global btn-home'} to={'/amount'}>
+          <BsFillArchiveFill />
+          Estoque
+        </NavLink>
+        <NavLink className={'btn-global btn-home'} to={'/profile'}>
+          <AiOutlineUser />
+          Perfil
+        </NavLink>
+      </div>
       <article>
-
-        <h1>Bem vindo a Gold Colchões, <span>{user.name}</span></h1>
         <Link to={'/newOrder'}>
           <button>Novo Pedido</button>
         </Link>
