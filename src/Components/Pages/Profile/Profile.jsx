8a1536c832878f6631773dff9b105360b593c338
Lucas Mendonça da Silva Junior <lucasjr413@gmail.com>
2023-10-17 23:37:06 -0300

    DESENV-015  - Finalizando Design lógica de validações no formulario

--- src/Components/Pages/Profile/Profile.jsx
+++ src/Components/Pages/Profile/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './Profile.css'
-import { FloatingLabel, Form, FormControl, Spinner } from 'react-bootstrap'
+import { FloatingLabel, Form, Spinner } from 'react-bootstrap'
 import { useForm } from 'react-hook-form'
 import { getUserLocalStorage } from '../../../context/util'
 import apiGold from '../../../Services/api'
@@ -26,6 +26,7 @@ const Profile = () => {
     const [confirmPasswordEnabled, setConfirmPasswordEnabled] = useState(false);
     const [passwordEqualsError, setpasswordEqualsError] = useState(false);
     const [emptyInputsError, setEmptyInputsError] = useState(false);
+    const [strongPasswordError, setStrongPasswordError] = useState(false);
     const [loading, setIsloading] = useState(null);
 
 
@@ -51,15 +52,21 @@ const Profile = () => {
     };
     const isFieldEmpty = (obj) => {
         return Object.keys(obj).length === 0;
-    }
-    const isPasswordMatching = () => {
-        const password = getValues('user_password');
-        const confirmPassword = getValues('confirm_password');
+    };
+    const isPasswordMatching = (password, confirmPassword) => {
         return password === confirmPassword;
+    };
+
+    const allInputsEmpty = (formData) => {
+        return !Object.keys(formData).some(k => formData[k] !== "");
     };
     const transitionAlert = (props) => {
         return <Slide {...props} direction="left" />;
-    }
+    };
+    const senhaForte = (password) => {
+        const pattern = /(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]).*(?=.*[A-Z]).*(?=.*[0-9]).{8,}/
+        return pattern.test(password)
+    };
 
     useEffect(() => {
         if (passwordEqualsError) {
@@ -74,13 +81,14 @@ const Profile = () => {
             }, 5000);
             return () => clearTimeout(timer);
         }
+        if (strongPasswordError) {
+            const timer = setTimeout(() => {
+                setStrongPasswordError(false);
+            }, 5000);
+            return () => clearTimeout(timer);
+        }
 
-    }, [passwordEqualsError, emptyInputsError]);
-
-    function allInputsEmpty() {
-        let form = getValues();
-        return !Object.keys(form).some(k => form[k] !== "");
-    }
+    }, [passwordEqualsError, emptyInputsError, strongPasswordError]);
 
     const enviarAlteracoes = async (e) => {
         e.preventDefault();
@@ -90,6 +98,7 @@ const Profile = () => {
             const name = getValues("user_name");
             const email = getValues("user_email");
             const pwd = getValues("user_password");
+            const pwdConfirm = getValues("confirm_password")
 
             let submittedData = {}
 
@@ -99,22 +108,25 @@ const Profile = () => {
                 password: pwd
             }
 
-            //validação de senhas iguais
-            if (!isPasswordMatching()) {
+            // validação de se todos os inputs estão nulos
+            if (allInputsEmpty(formValues)) {
+                setEmptyInputsError(true);
+                setIsloading(false);
+                return
+
+            } else if (!senhaForte(pwd) && confirmPasswordEnabled) { //validação senha forte 
+                setStrongPasswordError(true);
+                setValue("user_password", "");
+                setValue("confirm_password", "");
+                setIsloading(false);
+                return;
+            } else if (!isPasswordMatching(pwd, pwdConfirm) && confirmPasswordEnabled) {  //validação de senhas iguais
                 setpasswordEqualsError(true);
                 setValue("user_password", "");
                 setValue("confirm_password", "");
                 setIsloading(false);
                 return;
             }
-
-            // validação de se todos os inputs estão nulos
-            if (allInputsEmpty()) {
-                setEmptyInputsError(true);
-                setIsloading(false);
-                return
-            }
-
             //validação para retirar campos vazios do formulario no JSON
             for (const key in formValues) {
                 if (isFieldEmpty(formValues[key])) {
@@ -122,30 +134,21 @@ const Profile = () => {
                 }
                 submittedData = formValues;
             }
-            console.log(submittedData);
-            console.log(allInputsEmpty());
-            setIsDisable(true);
-            setIsloading(false);
-
-            reset({
-                user_name: "",
-                user_email: "",
-                user_password: "",
-                confirm_password: ""
-            })
-
-            // const id = browserUser.userId;
-            // const response = await apiGold.put(`Authenticate/Update/${id}`, profileData);
-            // if (response.data.success) {
-            //     console.log("DEU CERTO");
-            // }
-
+            const id = browserUser.userId;
+            const response = await apiGold.put(`Authenticate/Update/${id}`, submittedData);
+            if (response.data.success) {
+                setIsDisable(true);
+                setIsloading(false);
+                reset({
+                    user_name: "",
+                    user_email: "",
+                    user_password: "",
+                    confirm_password: ""
+                })
+                auth.logout()
+            }
         } catch (error) {
             console.log(error)
-        }
-        finally {
-            //reset(); 
-            //auth.logout()
         }
     }
 
@@ -154,17 +157,18 @@ const Profile = () => {
             <section className='profile_page d-flex flex-column justify-content-start align-items-center'>
                 <div className='display-user'><h2>Perfil</h2></div>
 
-                <Form onSubmit={enviarAlteracoes} className='profile_form d-flex flex-column justify-content-center '>
-                    <div className="form_header d-flex flex-column justify-content-center align-items-start">
-                        <h3>Dados do seu usuário</h3>
-                        <div className='description d-flex align-items-center'>
-                            <div className={`edit-description  d-${!isDisable ? 'flex ' : 'none'}`} >
+                <Form onSubmit={enviarAlteracoes} className='profile_form d-flex flex-column aligin-items-start justify-content-center '>
+                    <div className="form_header mb-3 d-flex flex-column justify-content-start align-items-center">
+                        <h3 className='mb-5'>Dados do seu usuário</h3>
+                        <div className='description mx-auto d-flex align-items-center'>
+                            <div className={`edit-description px-3 my-2  d-${!isDisable ? 'flex ' : 'none'}`} >
                                 <p>Não é necessário atualizar todos os campos.</p>
                                 <p>Ao concluir a alteração, você será deslogado.</p>
+                                <p>Senha: 1 caracter especial, 1 numero, 1 letra maiuscula e 8 caracteres no minimo. </p>
                             </div>
                         </div>
                     </div>
-                    <div>
+                    <div className='formInputs mx-auto'>
                         <FloatingLabel
                             label="Nome"
                             className="mb-3"
@@ -226,8 +230,13 @@ const Profile = () => {
                                 <Alert severity='error'> Ao menos um campo deve ser alterado </Alert>
                             </Snackbar>
                         )}
+                        {strongPasswordError && (
+                            <Snackbar open={strongPasswordError} anchorOrigin={{ horizontal: 'right', vertical: 'top' }} TransitionComponent={transitionAlert}>
+                                <Alert severity='error'> Senha no formato inválido. Insira uma senha forte! </Alert>
+                            </Snackbar>
+                        )}
                     </div>
-                    <div className='form-buttons d-flex justify-content-center mt-2'>
+                    <div className='form_buttons mx-auto d-flex justify-content-center mt-2'>
                         <button className={` btn btn-profile w-25 ${isDisable ? 'btn-outline-primary' : ' btn-outline-danger'}`} onClick={editForm}>
                             {isDisable ? 'Editar' : 'Cancelar'}
                         </button>
