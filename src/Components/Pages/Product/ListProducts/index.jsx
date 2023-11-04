4fe9d34b2f311d03929f0a51ca1ed1006328b550
Lucas Mendonça da Silva junior <lucasjr413@gmail.com>
2023-11-04 01:39:47 -0300

    DESENV-014 - Estilização inicial do layout das páginas de produto

--- src/Components/Pages/Product/ListProducts/index.jsx
+++ src/Components/Pages/Product/ListProducts/index.jsx
@@ -1,8 +1,29 @@
+import { useState } from 'react';
+import './ListProducts.css'
+import { Form } from 'react-bootstrap'
 
 const ListProducts = () => {
+
   return (
-    <div>ListProducts</div>
-  )
+    <section className='searchProductPage d-flex flex-column justify-content-start align-items-center'>
+      <div className="display-user">
+        <h1>Encontrar pedido</h1>
+      </div>
+      <div className='searchProduct w-25 d-flex justify-content-center' >
+        <Form.Control
+          type="text"
+          placeholder='Buscar produto...'
+        />
+        <button className='btn-global'>
+          Buscar
+        </button>
+      </div>
+
+      <div className='productsTable'>
+
+      </div>
+    </section>
+  );
 }
 
-export default ListProducts
+export default ListProducts;
--- src/Components/Pages/Product/NewProduct/index.jsx
+++ src/Components/Pages/Product/NewProduct/index.jsx
@@ -1,7 +1,44 @@
-
+import { Form } from 'react-bootstrap'
+import './NewProduct.css'
 const NewProduct = () => {
   return (
-    <div>newProduct</div>
+    <section className='NewProductPage d-flex flex-column justify-content-start align-items-center'>
+      <div className='display-user'><h2>Produto</h2></div>
+      
+      <form className="newProductForm d-flex flex-column justify-content-center my-5 p-5">
+        <h1 className="mb-4">Novo Produto</h1>
+        <div className="">
+          <Form.Group className="d-flex flex-column px-2 mb-4">
+            <label htmlFor="adr-postcode">Nome do produto</label>
+            <Form.Control
+            />
+          </Form.Group>
+          <Form.Group className="d-flex flex-column px-2 mb-4">
+            <label htmlFor="adr-postcode">Nome do produto</label>
+            <Form.Control
+            />
+          </Form.Group>
+          <div className='className="form_line d-flex mb-4 '>
+            <Form.Group className="d-flex flex-column px-2 w-25">
+              <label htmlFor="adr-postcode">Nome do produto</label>
+              <Form.Control
+              />
+            </Form.Group>
+            <Form.Group className="d-flex flex-column w-75 px-2">
+              <label htmlFor="adr-postcode">Nome do produto</label>
+              <Form.Select
+
+              >
+              <option value={""}>Selecione a forma de pagamento...</option>
+              <option value={""}>Selecione a forma de pagamento...</option>
+              <option value={""}>Selecione a forma de pagamento...</option>
+              </Form.Select>
+            </Form.Group>
+          </div>
+        </div>
+      </form>
+
+    </section>
   )
 }
 
