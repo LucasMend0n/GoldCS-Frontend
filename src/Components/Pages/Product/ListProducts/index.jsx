a12a295f82c3e3e34a7b5d508d74d4a89d409532
Lucas Mendonça da Silva Junior <lucasjr413@gmail.com>
2023-11-05 01:58:24 -0300

    DESENV - 014 - Tabela de produtos com ícone

--- src/Components/Pages/Product/ListProducts/index.jsx
+++ src/Components/Pages/Product/ListProducts/index.jsx
@@ -1,8 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './ListProducts.css'
 import { Form } from 'react-bootstrap'
+import { Table, TableBody, TableCell, TableFooter, TableHead, TableRow } from '@mui/material';
+import apiGold from '../../../../Services/api';
+import { AiFillEye } from 'react-icons/ai'
+import { TablePagination } from '@mui/base';
+
 
 const ListProducts = () => {
+  const [products, setProducts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await apiGold.get("/Product/WithoutPagination");
+      setProducts(response.data.result);
+    };
+    getProducts();
+  }, []);
+
+
+  const filteredProducts = products.filter(product => {
+    return product.name.toLowerCase().includes(searchTerm.toLowerCase());
+  });
 
   return (
     <section className='searchProductPage d-flex flex-column justify-content-start align-items-center'>
@@ -13,14 +33,27 @@ const ListProducts = () => {
         <Form.Control
           type="text"
           placeholder='Buscar produto...'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <button className='btn-global'>
-          Buscar
-        </button>
       </div>
 
-      <div className='productsTable'>
-
+      <div className='productsTable w-50'>
+        <Table>
+          <TableBody>
+            {filteredProducts.map((product) => (
+              <TableRow
+                key={product.productID}
+              >
+                <TableCell align='center' scope="product">{product.productID}</TableCell>
+                <TableCell align='center' scope="product">{product.name}</TableCell>
+                <TableCell align='center' scope='product'>{product.version}</TableCell>
+                <TableCell align='center' scope='product'>{product.price}</TableCell>
+                <TableCell align='center'><AiFillEye /></TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </div>
     </section>
   );
