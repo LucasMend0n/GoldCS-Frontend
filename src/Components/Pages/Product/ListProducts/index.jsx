41f01d50ea6abd18468489acb91ba711914abf76
Lucas Mendonça da Silva Junior <lucasjr413@gmail.com>
2023-11-13 14:25:23 -0300

    DESENV-014 - Modal de edição de produto com funcionalidades iniciais

--- src/Components/Pages/Product/ListProducts/index.jsx
+++ src/Components/Pages/Product/ListProducts/index.jsx
@@ -1,18 +1,20 @@
 import { useEffect, useState } from 'react';
 import './ListProducts.css'
-import { Button, Form, Modal } from 'react-bootstrap'
-import { Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow } from '@mui/material';
+import { Form, Modal, Spinner } from 'react-bootstrap'
 import apiGold from '../../../../Services/api';
 import { AiFillEye } from 'react-icons/ai'
-import ReactPaginate from 'react-paginate';
 import { NumericFormat } from 'react-number-format';
 import { useForm } from 'react-hook-form';
+import { BsFillTrashFill } from 'react-icons/bs'
 
 const ListProducts = () => {
   const [products, setProducts] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
-  const [selectedProduct, setSelectedProduct] = useState(null); // Estado para controlar o produto selecionado
-  const [showModal, setShowModal] = useState(false); // Estado para controlar a visibilidade do modal
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [showModalDelete, setShowModalDelete] = useState(false);
+  const [isDisable, setIsDisable] = useState(true);
+  const [loading, setIsloading] = useState(null);
 
   useEffect(() => {
     const getProducts = async () => {
@@ -28,16 +30,56 @@ const ListProducts = () => {
   const openModal = (product) => {
     setSelectedProduct(product);
     setShowModal(true);
+    reset({
+      product_id: product.productID,
+      product_name: product.name,
+      product_version: product.version,
+      product_categoryName: product.categoryName,
+      product_qtd: product.quantity,
+      product_price: product.price
+    });
   };
 
-  // Função para fechar o modal
   const closeModal = () => {
     setSelectedProduct(null);
     setShowModal(false);
   };
 
-  const form = useForm();
-  const { register, reset, getValues } = form;
+  const productForm = useForm({});
+
+  const { register, reset, getValues, setValue } = productForm;
+
+
+  const editForm = e => {
+    e.preventDefault();
+    setIsDisable(!isDisable)
+    if (isDisable === true) {
+      setValue("product_name", "")
+      setValue("product_version", "")
+      setValue("product_categoryName", "")
+      setValue("product_qtd", "")
+      setValue("product_price", "")
+    } else {
+      setValue("product_id", selectedProduct.productID)
+      setValue("product_name", selectedProduct.name)
+      setValue("product_version", selectedProduct.version)
+      setValue("product_categoryName", selectedProduct.categoryName)
+      setValue("product_qtd", selectedProduct.quantity)
+      setValue("product_price", selectedProduct.price)
+    }
+    setIsDisable(!isDisable);
+  }
+
+  const openDeleteConfirmation = () => {
+      setShowModalDelete(true)
+  }
+  const closeDeleteConfirmation = () => {
+      setShowModalDelete(false)
+  }
+
+  const deleteProduct = () => {
+
+  }
 
   return (
     <section className='searchProductPage d-flex flex-column justify-content-start align-items-center'>
@@ -76,6 +118,7 @@ const ListProducts = () => {
         </table>
       </div>
 
+      {/*Inicio do modal de editar e excluir*/}
       <Modal
         centered
         show={showModal}
@@ -83,70 +126,90 @@ const ListProducts = () => {
         backdrop="static"
       >
         <Modal.Header closeButton>
-          <Modal.Title>Detalhes do Produto</Modal.Title>
+          <Modal.Title>Dados do Produto</Modal.Title>
+          <button onClick={openDeleteConfirmation} className='deleteButton'><BsFillTrashFill /></button>
         </Modal.Header>
         <Modal.Body>
-          {selectedProduct && (
-            <>
-              <div className='line d-flex'>
-                <Form.Group className="d-flex flex-column px-2 mb-2 w-25">
-                  <label htmlFor="product_name">Id</label>
-                  <Form.Control
-                    value={selectedProduct.productID}
-                    type='text'
-                    disabled
-                  />
-                </Form.Group>
+          <>
+            <div className='line d-flex'>
+              <Form.Group className="d-flex flex-column px-2 mb-2 w-25">
+                <label>Id</label>
+                <Form.Control
+                  {...register("product_id")}
+                  type='text'
+                  disabled
+                />
+              </Form.Group>
 
-                <Form.Group className="d-flex flex-column px-2 mb-2 w-75">
-                  <label htmlFor="product_name">Nome</label>
-                  <Form.Control
-                    value={selectedProduct.name}
-                    type='text'
-                    disabled
-                  />
-                </Form.Group>
-                <Form.Group className="d-flex flex-column px-2 mb-2 w-50">
-                  <label htmlFor="product_name">Versao</label>
-                  <Form.Control
-                    value={selectedProduct.version}
-                    type='text'
-                    disabled
-                  />
-                </Form.Group>
-              </div>
-              <div className='line d-flex'>
-                <Form.Group className="d-flex flex-column px-2 mb-2 w-50">
-                  <label htmlFor="product_name">Categoria</label>
-                  <Form.Control
-                    value={selectedProduct.categoryName}
-                    type='text'
-                    disabled
-                  />
-                </Form.Group>
-                <Form.Group className="d-flex flex-column px-2 mb-2 w-75">
-                  <label htmlFor="product_name">Quantidade</label>
-                  <Form.Control
-                    value={selectedProduct.quantity}
-                    type='text'
-                    disabled
-                  />
-                </Form.Group>
-                <Form.Group className="d-flex flex-column px-2 mb-2 w-75">
-                  <label htmlFor="product_name">Preço</label>
-                  <Form.Control
-                    value={selectedProduct.price}
-                    type='text'
-                    disabled
-                  />
-                </Form.Group>
-              </div>
-            </>
-          )}
+              <Form.Group className="d-flex flex-column px-2 mb-2 w-75">
+                <label>Nome</label>
+                <Form.Control
+                  {...register("product_name")}
+                  type='text'
+                  disabled={isDisable}
+                />
+              </Form.Group>
+              <Form.Group className="d-flex flex-column px-2 mb-2 w-50">
+                <label>Versao</label>
+                <Form.Control
+                  {...register("product_version")}
+                  type='text'
+                  disabled={isDisable}
+
+                />
+              </Form.Group>
+            </div>
+            <div className='line d-flex'>
+              <Form.Group className="d-flex flex-column px-2 mb-2 w-50">
+                <label>Categoria</label>
+                <Form.Control
+                  {...register("product_categoryName")}
+                  type='text'
+                  disabled={isDisable}
+
+                />
+              </Form.Group>
+              <Form.Group className="d-flex flex-column px-2 mb-2 w-75">
+                <label>Quantidade</label>
+                <Form.Control
+                  {...register("product_qtd")}
+                  type='text'
+                  disabled={isDisable}
+
+                />
+              </Form.Group>
+              <Form.Group className="d-flex flex-column px-2 mb-2 w-75">
+                <label>Preço</label>
+                <Form.Control
+                  {...register("product_price")}
+                  type='text'
+                  disabled={isDisable}
+
+                />
+              </Form.Group>
+            </div>
+          </>
         </Modal.Body>
         <Modal.Footer>
-          
+          <button className={` btn btn-profile w-25 ${isDisable ? 'btn-outline-primary' : ' btn-outline-danger'}`} onClick={editForm}>
+            {isDisable ? 'Editar' : 'Cancelar'}
+          </button>
+          <button className="btn-global btn-profile w-25 mx-3" disabled={isDisable}>
+            {loading ? <Spinner> <span className="visually-hidden">Loading...</span> </Spinner> : <>Salvar</>}
+          </button>
         </Modal.Footer>
+      </Modal>
+      <Modal
+      onShow={showModalDelete}
+      onHide={closeDeleteConfirmation}
+      >
+        <p>Deseja realmente excluir o produto?</p>
+        <button className={`btn btn-profile w-25 btn-danger`}>
+          Sim
+        </button>
+        <button className="btn-global btn-profile btn-outline-danger w-25 mx-3" >
+         Cancelar
+        </button>
       </Modal>
     </section>
   );
